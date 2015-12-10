8b93a0e0a32e6888cac89e4f2e0896bb51bcf1a1
MHgeurts <geurts.maarten@gmail.com>
2015-12-10 12:53:38 +0100

    aanpassing 1
    aanpassing1

--- nanopore.py
+++ nanopore.py
@@ -1,36 +1,38 @@
 import pysam
 import glob
 import os
+import sys
+import shutil
 
-from time import time
-from time import sleep
+# from time import time
 import subprocess
-import multiprocessing as mp
-
 import argparse
 
 
-################### ---- Reference files ---- ####################
+
+#------------------------- subprocess directories ------------------------#
+
+poretools = "/usr/local/bin/poretools" 
+nanocorrectssss = "/usr/local/bin/nanocorrect" 
+
+
+#------------------------- Reference files -------------------------#
 
 human_ref = "route/to/directory"
 mitochondria_ref = "route/to/directory"
 
 
-################### ---- Reference files ---- ####################
-
-#
-
-################### ---- option input ---- ####################
+#------------------------- option input -------------------------#
 
 option = argparse.ArgumentParser(description="nanopore pipeline options")
 
-option.add_argument("files", metavar="fast5files", nargs="+", help="Specify the fast5 files directly, i.e. excecute this script in the right folder and select files using <runname>")
+option.add_argument("--inputdir", "-i", dest="indir", help="Specify the directory containing the fast5 files for analyzation directly")
+option.add_argument("--name", "-name", dest="name", help="Specify the runname")
 option.add_argument("--outdir","-o", dest="outdir", help="specify output directory for analysis", default="poo")
 option.add_argument("--type", "-t", dest="readtype", choices=["2D", "fw", "rev", "all"], help="Specify read type to be analyzed", default="2D")
 option.add_argument("--correct", "-c", dest="readcorrection", choices=["Correct", "NoCorrect"], help="Specify wether reads should be corrected utilizing nanocorrect", default="NoCorrect")
 option.add_argument("--mapping", "-m",  dest="mapping", choices=["lastal", "lastal-analyse", "graphmap-lin", "graphmap-cir"], help="Specify the read mapper", default="lastal")
-
-option= vars(option.parse_args())
+option = vars(option.parse_args())
 
 ###### DIT MOET IK NOG AANPASSEN, ALS IK HET GOED DOE HEB IK DEZE PAAR REGELS NIET NODIG #####
 
@@ -38,77 +40,82 @@ outdir = option["outdir"]
 mapping = option["mapping"]
 readtype = option["readtype"]
 readcorrection = option["readcorrection"]
+fast5dir = option["indir"] 
 
-################### ---- option input ---- ####################
+#------------------------- check input -------------------------#
 
-#
-
-################### ---- check input ---- ####################
-
-#
 
 def check_input():
 	f5count = 0 
-	for item in option["files"]:
-		if not os.path.isfile(item):
-			print "Fast5 files not properly supplied"+item
-			return False
-		else:
-			f5count += 1 
-	print f5count, "Fast5 files correctly specified"
-			
-	
-	if not os.path.exists(option["outdir"]):
-		os.makedirs(option["outdir"])
-		return True
-	else:
-		print "output directory already exists, please try again with new output directory name" 
+	dircount = 0
+	if len(glob.glob1(option["indir"], "*.fast5")) == 0:
+		print "No fast5 files in input directory, please specify correct input directory"
 		return False
+	else:
+		print len(glob.glob1(option["indir"], "*.fast5")), "fast5 files in input directory specified for analysis"
+	
+	while os.path.exists(option["outdir"]):
+		dircount +=1
+		option["outdir"] = option["outdir"]+"."+str(dircount)
+		print "output directory already exists and will be respecified"
+	else:
+		print "output directory:"+option["outdir"]
+		os.makedirs(option["outdir"])
+		return True 
 
-################### ---- check input ---- ####################
+#------------------------- read correction -------------------------#
 
-#
+def correction():
+	subprocess.call(["git", "clone", "https://github.com/jts/nanocorrect"])
+	subprocess.call(["make", "-f", "nanocorrect-overlap.make", "INPUT=../"+fastafile, "NAME="+option["name"]], cwd="nanocorrect")
+	subprocess.call(["python", "nanocorrect.py", option["name"], "all"], stdout=open(option["outdir"]+"/Fasta_fastQ"+"/corrected"+option["name"]+".fasta", "w+"), cwd="nanocorrect")
+	shutil.rmtree("nanocorrect")
+	
+	return True
+	
 
-################### ---- main pipeline ---- ####################
+#------------------------- main pipeline -------------------------#
 
 def main_pipeline():
-
 # fasta and fastq file extraction
-	os.makedirs(outdir/Fasta_Fastq)
-	poretools stats --type 
-	poretools fastq	--type option["readtype"] > outdir/Fasta_Fastq/outdir_Fastq
-	poretools fasta --type option["readtype"] > outdir/Fasta_Fastq/outdir_Fasta
+	os.makedirs(option["outdir"]+"/Fasta_fastQ")
+	fastafile = option["outdir"]+"/Fasta_fastQ"+"/"+option["name"]+".fasta"
+	subprocess.call(["poretools","stats","--type", option["readtype"] , "fast5/"])
+	subprocess.call(["poretools","fasta","--type", option["readtype"] , "fast5/"], stdout=open(fastafile, "w+"))
+	subprocess.call(["poretools","fastq","--type", option["readtype"] , "fast5/"], stdout=open(option["outdir"]+"/Fasta_fastQ"+"/"+option["name"]+".fastq", "w+"))
+	
+	
+	if option["readcorrection"] == "NoCorrect":
+		pass
+	else:
+		correction()
+		# make sure the corrected file is used in subsequent analysis
+		fastafile = option["outdir"]+"/Fasta_fastQ"+"/corrected_"+option["name"]+".fasta"
 
 
+	
+
+	
+	
+	
 
 
-
-
-
-
-
-
-
-################### ---- start phrases ---- ####################
-
-
-
-print "checking argument input"
-
-print "readtype = "+ readtype
-print "mapping = "+mapping
-print "readcorrection = "+ readcorrection
+#------------------------- start phrases -------------------------#
 
 if __name__ == "__main__":
+	print "checking argument input"
 	if check_input():
-		print "starting nanopore raw data analysis"
 		main_pipeline()
-	else:
+		
+	else: 
 		print("ERROR in command line input")
 
 
 
-################### ---- start phrases ---- ####################
+#------------------------- start phrases -------------------------#
+
+
+
 
 
 
