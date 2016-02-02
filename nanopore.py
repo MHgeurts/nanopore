1c0239d3b481eb87745f96a8ceb3488fec847146
MHgeurts <geurts.maarten@gmail.com>
2016-02-02 15:41:16 +0100

    Update nanopore.py

--- nanopore.py
+++ nanopore.py
@@ -101,7 +101,10 @@ else:
 	subprocess.call(["nanook_split_fasta", "-i", option["outdir"]+"/Fasta"+"/all_template.fasta", "-o", option["outdir"]+"/fasta"+"/Template"])
 	subprocess.call(["nanook_split_fasta", "-i", option["outdir"]+"/Fasta"+"/all_complement.fasta", "-o", option["outdir"]+"/fasta"+"/Complement"])
 subprocess.call(["nanook", "align", "-s", option["outdir"], "-r" ,human_ref, "-t", "24"])
-subprocess.call(["nanook", "analyse", "-s", option["outdir"], "-r" ,human_ref, "-t", "24"])
+if option["readtype"] == "2D":
+	subprocess.call(["nanook", "analyse", "-s", option["outdir"], "-r" ,human_ref, "-t", "24", "-2donly"])
+else:
+	subprocess.call(["nanook", "analyse", "-s", option["outdir"], "-r" ,human_ref, "-t", "24"])
 two_d = glob.glob(option["outdir"]+"/last"+"/2D"+"/*.maf")
 with open(option["outdir"]+"/last"+"/merged_2D_last.maf", "w+") as files_1:
 	lines = fileinput.input(two_d)
